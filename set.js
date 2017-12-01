de7c58c7970c471eefd250dd0b32eafafe1fe6a6
dawangong <dawangong@126.com>
2017-12-01 17:06:01 +0800

    半成品

---
+++ set.js
@@ -0,0 +1,72 @@
+(function (window) {
+    function Qset() {
+        this.arr=[];
+        this.index=0;
+    }
+    //add() 方法用来向一个Qset对象的末尾添加一个指定的值。
+    Qset.prototype.add=function (num) {
+        this.arr.push(num);
+        return this;
+    };
+    //clear() 方法用来清空一个Qset对象中的所有元素。
+    Qset.prototype.clear=function () {
+        this.arr=[];
+    };
+    //delete() 方法可以从一个Qset对象中删除指定的元素。
+    Qset.prototype.delete=function (num) {
+        let arrLength=this.arr.length;
+        this.arr.splice(num,1);
+        if(arrLength<this.arr.length){
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    //has() 方法返回一个布尔值来指示对应的值value是否存在Qset对象中
+    Qset.prototype.has=function (str) {
+        for (let i=0;i<this.arr.length;i++){
+            if (this.arr[i]==str){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    };
+
+    //entries()
+    Qset.prototype.entries=function () {
+        this.Iterator.arr=this.arr;
+        this.Iterator.index=this.index;
+        return this.Iterator;
+    };
+
+
+
+    Qset.prototype.forEach=function () {
+        
+    };
+
+
+    Qset.prototype.values=function () {
+
+    };
+
+    Qset.prototype.Iterator={
+        value:[],
+        next:function () {
+            let otherArr=[];
+            for (let i=0;i<2;i++){
+                otherArr.push(this.arr[this.index]);
+            }
+            this.index++;
+            this.value=otherArr;
+            return this;
+        }
+    };
+
+
+    window.Qset=Qset;
+})(window);
